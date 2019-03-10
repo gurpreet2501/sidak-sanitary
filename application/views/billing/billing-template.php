8868bb573db40a2e9e1afbcc5834e3c88879f455
Gurpreet Singh <gurpreet2501@gmail.com>
2019-03-10 11:53:32 +0530

    changes in templates

--- application/views/billing/billing-template.php
+++ application/views/billing/billing-template.php
@@ -1,14 +1,13 @@
-<link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/billing/bootstrap.min.css')?>"/>
+<link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/bootstrap.min.css')?>"/>
 <link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/billing/sb-admin.css')?>"/>
 <link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/billing/style.css')?>"/>
 <link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/billing/print.css')?>"/>
-<div class="container billing-temp">
+<div class="container-fluid billing-temp">
 	<div class="row">
 		<div class="col-xs-2"></div>	
 		<div class="col-xs-8">
 			<div class="text-center">
-				<h3 class="text-center"><strong>Tax Invoice</strong></h3>
-				<h1 class="text-center title"><strong>SIDAK INDUSTRIES</strong></h1>
+				<h3 class="text-center title"><strong>SIDAK INDUSTRIES</strong></h1>
 				<div class="sub-headings">D-174, Focal Point, Patiala.</div>
 				<div class="sub-headings">Mobile: 9814002872, +91-8872102872, Email: manmeetindustries1@gmail.com</div>
 				<br/>
@@ -20,92 +19,11 @@
 			<a class="no-print" onclick="window.print()"><button type="button" class="btn btn-danger no-print">Print</button></a>
 		</div>	
 	</div>
-<!-- --------------- -->
-
-<!-- --------------- -->
-
-<!-- --------------- -->
-<div class="row">
-  <div class="col-xs-1"></div>
-	<div class="col-xs-11">
-		<table class="table">
-			<tr>
-				<td>GSTIN: <strong>03AATPG4917F1ZS<strong></td>
-				<td>State: <strong>Punjab</strong></td>
-				<td>State Code: <strong>03</strong></td>
-			</tr>
-		</table>
-	</div>
-</div>
-<div class="row">
-
-	<div class="col-xs-5">
-		<table class="table"> 
-			<tr>
-				<td colspan="2" align="center">
-					<strong>Invoice Details</strong>
-				</td>
-			</tr>
-			<tr>
-				<td width="40%"></td>
-				<td> <strong>#4353</h4></td>
-			</tr>
-			<tr>
-				<td>Invoice Date:</td>
-				<td>Date</td>
-			</tr>
-			<tr>
-				<td class="address_field">Place Of Supply:</td>
-				<td></td>
-			</tr>
-			<tr>
-				<td class="address_field">Party's GSTIN No:</td>
-				<td></td>
-			</tr>
-		</table>
-	</div>
-	<div class="col-xs-5">
-		<!-- Right Side -->
-		<table class="table">
-			<tr>
-				<td colspan="2" align="center">
-					<strong>Details of Consignment | Shipped To</strong>
-				</td>
-			</tr>
-			<tr>
-				<td width="30%">
-					Vehicle No:
-				</td>
-				<td>
-					
-				</td>
-			</tr>
-
-			<tr>
-				<td class="address_field">
-					Time:
-				</td>
-				<td>
-					<?=date("H:i:s")?>
-				</td>
-			</tr>
-			<tr>
-				<td>Mode Of Dispatch:</td>
-				<td></td>
-			</tr>
-			</table>
-		<!-- Right Side ends -->
-	</div>
-	<div class="col-xs-1"></div>
-</div>
-<!-- --------------- -->
 
 	<div class="row">
-		<div class="col-xs-1"></div>
-		<div class="col-xs-10">
-		 <h4>Billing Items</h4>
+		<div class="col-xs-12">
 			<div class="billing_items">
-					<table class="table table-stripped" border="1px" cellpadding="8px">
+					<table class="table table-bordered">
 						<tr>
 							<td align="center"><strong>Sno</strong></th>
 							<td align="center"><strong>Item</strong></th>
@@ -123,6 +41,54 @@
 								<td align="center">43</td>
 								<td align="center">3453653</td>
 							</tr>
+								<tr>
+								<td align="center">1</td>
+								<td align="center">Cera Seats</td>
+								<td align="center">Sk34354</td>
+								<td align="center">435</td>
+								<td align="center">43</td>
+								<td align="center">3453653</td>
+							</tr>
+								<tr>
+								<td align="center">1</td>
+								<td align="center">Cera Seats</td>
+								<td align="center">Sk34354</td>
+								<td align="center">435</td>
+								<td align="center">43</td>
+								<td align="center">3453653</td>
+							</tr>
+								<tr>
+								<td align="center">1</td>
+								<td align="center">Cera Seats</td>
+								<td align="center">Sk34354</td>
+								<td align="center">435</td>
+								<td align="center">43</td>
+								<td align="center">3453653</td>
+							</tr>
+								<tr>
+								<td align="center">1</td>
+								<td align="center">Cera Seats</td>
+								<td align="center">Sk34354</td>
+								<td align="center">435</td>
+								<td align="center">43</td>
+								<td align="center">3453653</td>
+							</tr>
+								<tr>
+								<td align="center">1</td>
+								<td align="center">Cera Seats</td>
+								<td align="center">Sk34354</td>
+								<td align="center">435</td>
+								<td align="center">43</td>
+								<td align="center">3453653</td>
+							</tr>
+								<tr>
+								<td align="center">1</td>
+								<td align="center">Cera Seats</td>
+								<td align="center">Sk34354</td>
+								<td align="center">435</td>
+								<td align="center">43</td>
+								<td align="center">3453653</td>
+							</tr>
 						
 					
 					</table>
@@ -132,9 +98,8 @@
 			
 	</div>
 	<div class="row">
-		<div class="col-xs-1"></div>
-		<div class="col-xs-6">
-			<table class="table">
+		<div class="col-xs-7">
+			<table class="table table-bordered">
 					<tr>
 						<td>Bank Details: </td>
 						<td><strong>INDIAN BANK, PATIALA</strong></td>
@@ -149,7 +114,7 @@
 					</tr>
 				</table>
 		
-				<table class="table">
+				<table class="table table-bordered">
 					<tr>
 						<td>Bank Details:</td>
 						<td><strong>ORIENTAL BANK  OF COMMERCE, PATIALA</strong></td>
@@ -165,9 +130,9 @@
 				</table>
 		
 		</div> <!-- col-xs-6 -->
-		<div class="col-xs-4">
-		<div class="col-xs-1"></div>
-			<table class="table">
+		<div class="col-xs-5">
+	
+			<table class="table table-bordered">
 					<tr>
 						<td><strong>Total Amount Before Tax:</strong></td>
 						<td>Rs.34</td>
@@ -197,10 +162,7 @@
 						<td>Rs.45</td>
 					</tr>
 			</table>
-			<blockquote>
-			 <footer>Total invoice amount in words</footer>
-			  <p><?=ucfirst(str_replace('-',' ',convert_number_to_words(ceil(534533))))?> Only</p>
-			</blockquote>
+			
 			
 		</div>
 	</div>
--- application/views/billing/terms-and-conditions.php
+++ application/views/billing/terms-and-conditions.php
@@ -1,25 +1,15 @@
 	<div class="row">
-		<div class="col-xs-1"></div>
-		<div class="col-xs-5">
+		<div class="col-xs-6">
 			<div class="black-border">
 				Terms and Conditions
 				<ol>
 					<li>Goods once sold will not be taken back</li>
 					<li>Interest @ 2% per month will be charged if payment is not made with in 7 days from the date of bill</li>
-					<li>Subject to Derabassi Juridiction only</li>
 					<li>Only Responsibility ceases when the goods are handed over in sound condition</li>
 				</ol>
 			</div>
 		</div>
-		<div class="col-xs-2">
-			<div class="black-border">
-				<br/>	
-				<br/>	
-				<br/>	
-				Customers Signature
-			</div>
-		</div>
-		<div class="col-xs-3">
+		<div class="col-xs-6">
 			<div class="black-border">
 				<p>Certified that particulars given above are true and correct</p>
 				 <h3>For SIDAK INDUSTRIES</h3>
